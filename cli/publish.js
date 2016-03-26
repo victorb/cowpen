bf92f64e327b6fa4c916685d7ae5359bf892b563
Victor Bjelkholm <victor@typeform.com>
2016-03-26 12:29:51 -0500

    Refactor

--- cli/publish.js
+++ cli/publish.js
@@ -1,4 +1,5 @@
 var IPFSApi = require('ipfs-api')
+var path = require('path')
 
 const unify = (name, version) => {
   return name + '@' + version
@@ -14,17 +15,20 @@ const findHashFromName = (hashes, name) => {
   return to_return
 }
 
+const addToIPFS = (directory, pkg_name, callback) => {
+  const ipfs = new IPFSApi('localhost', '5001')
+  ipfs.add(directory, {recursive: true}, (err, res) => {
+    if (err) {
+      throw new Error(err)
+    }
+    callback(findHashFromName(res, pkg_name))
+  })
+}
+
 module.exports = (args) => {
   const pkg_json = require(process.cwd() + '/package.json')
   const pkg_name = pkg_json.name
   const pkg_version = pkg_json.version
   console.log('Publishing ' + unify(pkg_name, pkg_version))
-
-  const ipfs = new IPFSApi('localhost', '5001')
-  ipfs.add('.', {recursive: true}, (err, res) => {
-    if (err) {
-      throw new Error(err)
-    }
-    console.log(findHashFromName(res, pkg_name))
-  })
+  addToIPFS('.', pkg_name, console.log)
 }
