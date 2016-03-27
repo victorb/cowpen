d389fb713bc0d43f1bf10622b3b8e828169b5538
Victor Bjelkholm <victor@typeform.com>
2016-03-26 19:16:42 -0500

    Finish publish

--- cli/publish.js
+++ cli/publish.js
@@ -1,5 +1,10 @@
 var IPFSApi = require('ipfs-api')
 var path = require('path')
+var mkdirp = require('mkdirp')
+var packages = require('../lib/packages')
+
+const COWPEN_PATH = '/Users/victor/.cowpen'
+const COWPEN_PACKAGES_PATH = COWPEN_PATH + '/packages.json'
 
 const unify = (name, version) => {
   return name + '@' + version
@@ -15,20 +20,51 @@ const findHashFromName = (hashes, name) => {
   return to_return
 }
 
-const addToIPFS = (directory, pkg_name, callback) => {
+const addToIPFS = (directory, include_hidden, callback) => {
   const ipfs = new IPFSApi('localhost', '5001')
-  ipfs.add(directory, {recursive: true}, (err, res) => {
+  ipfs.add(directory, {recursive: true, hidden: include_hidden}, (err, res) => {
     if (err) {
       throw new Error(err)
     }
-    callback(findHashFromName(res, pkg_name))
+    callback(res)
   })
 }
 
+const publishToIPFS = (hash, callback) => {
+  const ipfs = new IPFSApi('localhost', '5001')
+  ipfs.name.publish(hash, {recursive: true}, (err, res) => {
+    if (err) {
+      throw new Error(err)
+    }
+    callback(res.Name, res.Value)
+  })
+}
+
+const addToPublicIndex = (packages_path, hash, pkg_json, callback) => {
+  const opts = Object.assign({}, pkg_json, {hash})
+  packages.addPackage(packages_path, opts)
+  callback()
+}
+
 module.exports = (args) => {
+  mkdirp.sync(COWPEN_PATH)
   const pkg_json = require(process.cwd() + '/package.json')
   const pkg_name = pkg_json.name
   const pkg_version = pkg_json.version
   console.log('Publishing ' + unify(pkg_name, pkg_version))
-  addToIPFS('.', pkg_name, console.log)
+  addToIPFS(process.cwd(), false,(res) => {
+    const hash = findHashFromName(res, pkg_name)
+    console.log('Added to IPFS, got hash ' + hash)
+    addToPublicIndex(COWPEN_PACKAGES_PATH, hash, pkg_json, () => {
+      console.log('Added package to packages.json, adding to IPFS')
+      addToIPFS(COWPEN_PATH, true, (res) => {
+        const index_hash = findHashFromName(res, path.basename(COWPEN_PATH))
+        console.log('Added to packages.json. Got hash ' + index_hash + '. Publishing to IPNS')
+        publishToIPFS(index_hash, (name, value) => {
+          console.log('Published!')
+          console.log('Woho! ' + name + ' now points to ' + value)
+        })
+      })
+    })
+  })
 }
